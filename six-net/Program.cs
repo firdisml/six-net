fb5ab149433796c55d54236da995eb4c94739c61
Firdaus Ismail <44713893+firdisml@users.noreply.github.com>
2023-02-27 14:58:17 +0800

    Added Connection String

--- six-net/Program.cs
+++ six-net/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration.UserSecrets;
 using six_net.Data;
 using six_net.Interfaces;
 using six_net.Services;
@@ -13,10 +14,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ContestInterface, ContestService>();
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
-builder.Services.AddDbContext<DataContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddDbContext<DataContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("Host=containers-us-west-72.railway.app:7297;Username=postgres;Password=1kTOkH7pHCyhpI9fpEvB;Database=railway")));
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
